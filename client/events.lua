a3d80c4750ec16e1bb981ff6985cdc1401a326d3
MaDHouSe79 <madhouse1979@gmail.com>
2022-01-22 21:15:59 +0100

    Update events.lua

--- client/events.lua
+++ client/events.lua
@@ -1,5 +1,5 @@
 AddEventHandler("qb-aimedic:client:loadDockter", function()
-    player    = GetPlayerPed(-1)
+    player          = GetPlayerPed(-1)
     local playerPos = GetEntityCoords(player)
     local dokter    = GetHashKey(Config.Ped['ambulance'].model)
     RequestModel(dokter)
@@ -8,9 +8,9 @@ AddEventHandler("qb-aimedic:client:loadDockter", function()
         Citizen.Wait(0)
     end
     if DoesEntityExist(dokter) then
-		NPCDoktor(playerPos.x, playerPos.y, playerPos.x, dokter)
-	else
-		NPCDoktor(playerPos.x, playerPos.y, playerPos.x, dokter)
-	end
-	ClearPedTasksImmediately(player)
+	NPCDoktor(playerPos.x, playerPos.y, playerPos.x, dokter)
+    else
+	NPCDoktor(playerPos.x, playerPos.y, playerPos.x, dokter)
+    end
+    ClearPedTasksImmediately(player)
 end)
