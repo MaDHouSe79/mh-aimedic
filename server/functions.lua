a46cf01272da58641c4ee114ed5239300f305f55
MaDHouSe79 <madhouse1979@gmail.com>
2022-01-22 21:16:30 +0100

    Update functions.lua

--- server/functions.lua
+++ server/functions.lua
@@ -1,6 +1,6 @@
 function SocietyAccount(job)
     if Config.UseSocietyAccount then 
-	    TriggerEvent('qb-bossmenu:server:addAccountMoney', job, Config.treatCost)
+        TriggerEvent('qb-bossmenu:server:addAccountMoney', job, Config.treatCost)
     end
     TriggerClientEvent('hospital:client:SendBillEmail', job, Config.treatCost)
 end
