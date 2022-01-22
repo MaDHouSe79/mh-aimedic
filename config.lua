a9ced796db620fff3808cb8b5ecff79c3c120a30
MaDHouSe79 <madhouse1979@gmail.com>
2022-01-22 21:20:39 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,5 +1,5 @@
 Config = {}
-Config.Locale            = 'nl' -- change to en or nl for now
+Config.Locale            = 'nl'    -- change to en or nl for now
 Config.NotifyShowTime    = 5000
 Config.MoneyFormat       = '€'  -- $ or € or your ond format
 Config.UseSocietyAccount = true
@@ -7,15 +7,16 @@ Config.GradePayment      = 2
 Config.MinOnLineDoktors  = 2 
 Config.treatCost         = 2000
 Config.spawnRadius       = 100
+
 Config.Ped = {
     ['ambulance'] = {
-        company      = "Eerste Hulp",
-        name         = "Doktor bibber", 
-        model        = "s_m_m_doctor_01",
-        job          = 'ambulance', 
-        vehicle      = "ambumicu",    
-        colour       = 111,
-        spawnRadius  = 100,              -- Default Value: 100
-        drivingStyle = 786603,           -- Default Value: 786603
+        company      = "Eerste Hulp",      -- company name used for sending mails
+        name         = "Doktor bibber",    -- The name of the docter using for sending mail
+        model        = "s_m_m_doctor_01",  -- The model bed to spawn...
+        job          = 'ambulance',        -- Not active tet...
+        vehicle      = "ambumicu",         -- Not active tet...    
+        colour       = 111,                -- Not active tet...
+        spawnRadius  = 100,                -- Default Value: 100
+        drivingStyle = 786603,             -- Default Value: 786603
     },
 }
