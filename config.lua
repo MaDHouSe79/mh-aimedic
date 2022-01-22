c39af378bb878b1c95a81421e5ebfc6ab19e232c
MaDHouSe79 <madhouse1979@gmail.com>
2022-01-22 22:36:51 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,12 +1,12 @@
 Config = {}
-Config.Locale            = 'nl'            -- Change to en or nl for now
-Config.NotifyShowTime    = 5000            -- All notivication display time on screen (5000 is 5 secs)
-Config.MoneyFormat       = '€'             -- $ or € or your ond format
-Config.UseSocietyAccount = true            -- With true the Society Account gets the money.
-Config.GradePayment      = 2               -- Not active yet
-Config.MinOnLineDoktors  = 2               -- How many ambulance jobs have to be online to make this work, you can set this to 0 if you have a huge server and les time to get to players.
-Config.treatCost         = 2000            -- The treat costs (change if needed)
-Config.spawnRadius       = 100             -- Not active yet
+Config.Locale            = 'nl'                -- Change to en or nl for now
+Config.NotifyShowTime    = 5000                -- All notivication display time on screen (5000 is 5 secs)
+Config.MoneyFormat       = '€'                 -- $ or € or your ond format
+Config.UseSocietyAccount = true                -- With true the Society Account gets the money.
+Config.GradePayment      = 2                   -- Not active yet
+Config.MinOnLineDoktors  = 2                   -- How many ambulance jobs have to be online to make this work, you can set this to 0 if you have a huge server and les time to get to players.
+Config.treatCost         = 2000                -- The treat costs (change if needed)
+Config.spawnRadius       = 100                 -- Not active yet
 
 Config.Ped = {
     ['ambulance'] = {
