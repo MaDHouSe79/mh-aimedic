9c83dbab36af6e595c075a692b2a76ef90e37365
MaDHouSe79 <madhouse1979@gmail.com>
2022-01-22 22:06:27 +0100

    Update en.lua

--- locales/en.lua
+++ locales/en.lua
@@ -1,13 +1,13 @@
 local Translations = {
     error = {
-        not_enough_money = "You can't afford medical treatment",
-        only_when_dead = "This can only be used when dead",
-        to_many_medics = "There is to many medics online",
-        job_not_found = "Error: no job %value found in job shared files of qbcore",
-        unknow = "Unknow error...",
+        not_enough_money  = "You can't afford medical treatment",
+        only_when_dead    = "This can only be used when dead",
+        to_many_medics    = "There is to many medics online",
+        job_not_found     = "Error: no job %value found in job shared files of qbcore",
+        unknow_error      = "Unknow error...",
     },
     success = {
-        treatment_done = "Your treatment is done, you were charged: %value",
+        treatment_done    = "Your treatment is done, you were charged: %value",
     },
     info = {
         getting_treatment = "The doctor is giving you medical aid",
