735a5a05b0039cb326d97df536ae919dadbcfe52
L1quidSn4ke <104674262+L1quidSn4ke@users.noreply.github.com>
2022-05-16 17:30:13 +0900

    Update en.lua
    Fixed spelling and grammar errors

--- locales/en.lua
+++ locales/en.lua
@@ -2,7 +2,7 @@ local Translations = {
     error = {
         not_enough_money  = "You can't afford medical treatment",
         only_when_dead    = "This can only be used when dead",
-        to_many_medics    = "There is to many medics online",
+        to_many_medics    = "There are too many medics online",
         job_not_found     = "Error: no job %value found in job shared files of qbcore",
         unknow_error      = "Unknow error...",
     },
@@ -11,12 +11,12 @@ local Translations = {
     },
     info = {
         getting_treatment = "The doctor is giving you medical aid",
-        waiting_message   = "You must wait until the dockter if ready",
+        waiting_message   = "You must wait until the doctor is ready",
     },
     mail = {
-        sender            = "%{docter}",
+        sender            = "%{doctor}",
         subject           = "Medical assistance",
-        message           = "Hey, %{username}<br /><br />A invoice for Medical assistance!<br />Location: <strong>Garage %{streetName}</strong><br /><br/><br/>Kind regard,<br />%{company}",
+        message           = "Hey, %{username}<br /><br />An invoice for Medical assistance!<br />Location: <strong>Garage %{streetName}</strong><br /><br/><br/>Kind regard,<br />%{company}",
     },
 }
 
