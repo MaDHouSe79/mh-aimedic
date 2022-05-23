abb95d594d82877f555917e01530724c0c5111e7
MaDHouSe79 <madhouse1979@gmail.com>
2022-05-23 23:26:29 +0200

    Delete functions.lua

--- server/functions.lua
+++
@@ -1,6 +0,0 @@
-function SocietyAccount(job)
-    if Config.UseSocietyAccount then 
-        TriggerEvent('qb-bossmenu:server:addAccountMoney', job, Config.treatCost)
-    end
-    TriggerClientEvent('hospital:client:SendBillEmail', job, Config.treatCost)
-end
