9de7d92eb439231b81220aa0ae427314699c714b
MaDHouSe79 <madhouse1979@gmail.com>
2022-05-23 23:26:20 +0200

    Update main.lua

--- server/main.lua
+++ server/main.lua
@@ -1,1 +1,49 @@
-QBCore = exports['qb-core']:GetCoreObject()
+local  QBCore = exports['qb-core']:GetCoreObject()
+
+local function SocietyAccount(job)
+    if Config.UseSocietyAccount then 
+        TriggerEvent('qb-bossmenu:server:addAccountMoney', job, Config.treatCost)
+    end
+    TriggerClientEvent('hospital:client:SendBillEmail', job, Config.treatCost)
+end
+
+QBCore.Functions.CreateCallback('qb-aimedic:server:GetOnlineEMS', function(source, cb, job)
+    local result = QBCore.Functions.GetDutyCount(job)
+    if result ~= nil or result >= 0 then
+        cb({
+            status = true,
+            online = result
+        })
+    else
+        cb({
+            status = false,
+            online = 0
+        })
+    end
+end)
+
+QBCore.Functions.CreateCallback('qb-aimedic:server:PayJob', function(source, cb, job)
+    local Player = QBCore.Functions.GetPlayer(source)
+    if Player.Functions.RemoveMoney("cash", Config.treatCost, "revived-player") then
+        TriggerClientEvent('hospital:client:Revive', source)
+        SocietyAccount(job)
+        cb({
+            status = true, 
+            message = Lang:t('info.getting_treatment') 
+        })
+    else
+        if Player.Functions.RemoveMoney("bank", Config.treatCost, "revived-player") then
+            TriggerClientEvent('hospital:client:Revive', source)
+            SocietyAccount(job)
+            cb({
+                status = true, 
+                message = Lang:t('info.getting_treatment')
+            })
+        else
+            cb({
+                status = false, 
+                message = Lang:t('error.not_enough_money')
+            })
+        end
+    end
+end)
